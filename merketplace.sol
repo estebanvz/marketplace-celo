526b8db704c4b031a5cf174d3e0e21c1e7b0f93d
esteban vilca <esteban.wilfredo.g@gmail.com>
2022-09-12 22:27:52 -0300

    [FEAT] adding read and write to the product contract

--- merketplace.sol
+++ merketplace.sol
@@ -4,5 +4,11 @@ pragma solidity >=0.7.0 <0.9.0;
 
 contract Marketplace {
     string internal product;
+    function writeProduct(string memory _product) public{
+        product=_product;
+    }
 
+    function readProduct() public view returns(string memory){
+        return product;
+    }
 }
